8286d17fba21bb957ab8d344b51a78af0ef39f68
baraklevy20 <baraklevy20@gmail.com>
2019-01-10 17:04:36 +0200

    Update word_graph.py

--- word_graph.py
+++ word_graph.py
@@ -38,7 +38,9 @@ def generate_word_graph():
         # todo take only most frequent
         for word in words:
             if not G.has_node(word):
-                G.add_node(word)
+                G.add_node(word, weight=1)
+            else:
+                G.node[word]['weight'] += 1
 
         for w1 in words:
             for w2 in words:
@@ -47,6 +49,10 @@ def generate_word_graph():
                         G.add_edge(w1, w2, weight=0.5)
                     else:
                         G[w1][w2]['weight'] += 0.5
+
+    for edge in G.edges:
+        G[edge[0]][edge[1]]['weight'] /= max(G.node[edge[0]]['weight'], G.node[edge[1]]['weight'])
+
 
     # G.add_node('avi')
     # G.add_edge('avi', 'mazor', weight=1)
@@ -58,20 +64,16 @@ def generate_word_graph():
 
 
 def get_word_similarity(G, word1, word2):
-    shortest_paths = nx.all_shortest_paths(G, word1, word2)
-    max = 0
-    # todo change remove try catch
-    try:
-        for path in shortest_paths:
-            for i in range(len(path) - 1):
-                if G[path[i]][path[i + 1]]['weight'] > max:
-                    max = G[path[i]][path[i + 1]]['weight']
-            # print(path)
-    except Exception:
+    if not G.has_node(word1) or not G.has_node(word2):
         return 0
 
-    # print(max)
-    return max
+    shortest_path = nx.shortest_path(G, word1, word2)
+    sum = 0
+
+    for i in range(len(shortest_path) - 1):
+        sum += G[shortest_path[i]][shortest_path[i + 1]]['weight']
+
+    return sum / (len(shortest_path) - 1)
 
 
 def get_article_similarity(G, article1, article2):
@@ -89,6 +91,8 @@ def get_article_similarity(G, article1, article2):
 
     return weight_sum / len(set(articles1_words) | set(articles2_words))
 
+
+G = generate_word_graph()
 # edge_labels = dict([((u,v,),d['weight'])
 #                  for u,v,d in G.edges(data=True)])
 # pos=nx.spring_layout(G)
@@ -97,8 +101,10 @@ def get_article_similarity(G, article1, article2):
 # plt.show()
 
 
-G = generate_word_graph()
-print(get_article_similarity(G, 'high soil acidity, very low of nutrient availability  especially NPK', 'fruit trees agricultural'))
-print(get_article_similarity(G, 'In the engineering curriculum, remote labs', 'agricultural fruit'))
-print(get_article_similarity(G, 'In the engineering curriculum, remote labs', 'high soil acidity, very low of nutrient availability  especially NPK'))
-# print(get_article_similarity(G, ""))
+# print(get_article_similarity(G, 'tomer tomer', 'barak mazor'))
+# print(get_article_similarity(G, 'high soil acidity, very low of nutrient availability  especially NPK', 'fruit trees agricultural'))
+# print(get_article_similarity(G, 'In the engineering curriculum, remote labs', 'agricultural fruit'))
+# print(get_article_similarity(G, 'In the engineering curriculum, remote labs', 'high soil acidity, very low of nutrient availability  especially NPK'))
+print(get_article_similarity(G, 'physical characteristicsare among propertiesaffecting the susceptibility to erosion', 'high soil acidity, very low of nutrient availability  especially NPK'))
+# print(get_article_similarity(G, 'This paper focuses on a new way for students to deepen their knowledge in image processing', 'high soil acidity, very low of nutrient availability  especially NPK'))
+# print(get_article_similarity(G, 'This paper focuses on a new way for students to deepen their knowledge in image processing', 'In the engineering curriculum, remote labs'))
